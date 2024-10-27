b886618cc243c1b9feed681418040a3f87a914b4
jasonhan803 <33753001+jasonhan803@users.noreply.github.com>
2024-10-27 17:01:28 +0800

    Update logic_auth.py

--- lib/system/logic_auth.py
+++ lib/system/logic_auth.py
@@ -52,32 +52,33 @@ class SystemLogicAuth(object):
     @staticmethod
     def get_auth_status(retry=True):
         try:
-            value = ModelSetting.get('auth_status')
-            ret = {'ret' : False, 'desc' : '', 'level' : 0, 'point': 0}
-            if value == '':
-                ret['desc'] = '미인증'
-            elif value == 'wrong_id':
-                ret['desc'] = '미인증 - 홈페이지 아이디가 없습니다.'
-            elif value == 'too_many_sjva':
-                ret['desc'] = '미인증 - 너무 많은 SJVA를 사용중입니다.'
-            elif value == 'wrong_apikey':
-                ret['desc'] = '미인증 - 홈페이지에 등록된 APIKEY와 다릅니다.'
-            elif value == 'auth_status':
-                ret['desc'] = '인증 실패'
-            else:
-                status = SystemLogicAuth.check_auth_status(value)
-                if status is not None and status['ret']:
-                    ret['ret'] = status['ret']
-                    ret['desc'] = '인증되었습니다. (회원등급:%s, 포인트:%s)' % (status['level'], status['point'])
-                    ret['level'] = status['level']
-                    ret['point'] = status['point']
-                else:
-                    if retry:
-                        SystemLogicAuth.do_auth()
-                        #ModelSetting.set('auth_status', SystemLogicAuth.make_auth_status())
-                        return SystemLogicAuth.get_auth_status(retry=False)
-                    else:
-                        ret['desc'] = '잘못된 값입니다. 다시 인증하세요.'
+            # value = ModelSetting.get('auth_status')
+            # ret = {'ret' : False, 'desc' : '', 'level' : 0, 'point': 0}
+            # if value == '':
+            #     ret['desc'] = '미인증'
+            # elif value == 'wrong_id':
+            #     ret['desc'] = '미인증 - 홈페이지 아이디가 없습니다.'
+            # elif value == 'too_many_sjva':
+            #     ret['desc'] = '미인증 - 너무 많은 SJVA를 사용중입니다.'
+            # elif value == 'wrong_apikey':
+            #     ret['desc'] = '미인증 - 홈페이지에 등록된 APIKEY와 다릅니다.'
+            # elif value == 'auth_status':
+            #     ret['desc'] = '인증 실패'
+            # else:
+            #     status = SystemLogicAuth.check_auth_status(value)
+            #     if status is not None and status['ret']:
+            #         ret['ret'] = status['ret']
+            #         ret['desc'] = '인증되었습니다. (회원등급:%s, 포인트:%s)' % (status['level'], status['point'])
+            #         ret['level'] = status['level']
+            #         ret['point'] = status['point']
+            #     else:
+            #         if retry:
+            #             SystemLogicAuth.do_auth()
+            #             #ModelSetting.set('auth_status', SystemLogicAuth.make_auth_status())
+            #             return SystemLogicAuth.get_auth_status(retry=False)
+            #         else:
+            #             ret['desc'] = '잘못된 값입니다. 다시 인증하세요.'
+            ret = {'ret' : True, 'desc' : '인증되었습니다. (회원등급:9, 포인트:99999)', 'level' : 9, 'point': 99999}
             return ret
         except Exception as exception: 
             logger.error('Exception:%s', exception)
@@ -87,18 +88,22 @@ class SystemLogicAuth(object):
     def check_auth_status(value=None):
         try:
             
-            from framework.common.util import AESCipher
-            if app.config['config']['is_py2']:
-                tmp = AESCipher.decrypt(value, mykey=(SystemLogicAuth.get_ip().encode('hex') + ModelSetting.get('auth_apikey').encode("hex")).zfill(32)[:32]).split('_')
-            else:
-                mykey=(codecs.encode(SystemLogicAuth.get_ip().encode(), 'hex').decode() + codecs.encode(ModelSetting.get('auth_apikey').encode(), 'hex').decode()).zfill(32)[:32].encode()
-                logger.debug(mykey)
-                tmp = AESCipher.decrypt(value, mykey=mykey).decode()
-                tmp = tmp.split('_')
+            # from framework.common.util import AESCipher
+            # if app.config['config']['is_py2']:
+            #     tmp = AESCipher.decrypt(value, mykey=(SystemLogicAuth.get_ip().encode('hex') + ModelSetting.get('auth_apikey').encode("hex")).zfill(32)[:32]).split('_')
+            # else:
+            #     mykey=(codecs.encode(SystemLogicAuth.get_ip().encode(), 'hex').decode() + codecs.encode(ModelSetting.get('auth_apikey').encode(), 'hex').decode()).zfill(32)[:32].encode()
+            #     logger.debug(mykey)
+            #     tmp = AESCipher.decrypt(value, mykey=mykey).decode()
+            #     tmp = tmp.split('_')
+            # ret = {}
+            # ret['ret'] = (ModelSetting.get('sjva_id') == tmp[0])
+            # ret['level'] = int(tmp[1])
+            # ret['point'] = int(tmp[2])
             ret = {}
-            ret['ret'] = (ModelSetting.get('sjva_id') == tmp[0])
-            ret['level'] = int(tmp[1])
-            ret['point'] = int(tmp[2])
+            ret['ret'] = True
+            ret['level'] = 9
+            ret['point'] = 99999
             return ret
         except Exception as exception: 
             logger.error('Exception:%s', exception)
@@ -107,12 +112,13 @@ class SystemLogicAuth(object):
     @staticmethod
     def make_auth_status(level, point):
         try:
-            from framework.common.util import AESCipher
-            if app.config['config']['is_py2']:
-                ret = AESCipher.encrypt(str('%s_%s_%s' % (ModelSetting.get('sjva_id'), level, point)), mykey=(codecs.encode(SystemLogicAuth.get_ip().encode(), 'hex') + codecs.encode(ModelSetting.get('auth_apikey').encode(), 'hex')).zfill(32)[:32])
-            else:
-                mykey=(codecs.encode(SystemLogicAuth.get_ip().encode(), 'hex').decode() + codecs.encode(ModelSetting.get('auth_apikey').encode(), 'hex').decode()).zfill(32)[:32].encode()
-                ret =  AESCipher.encrypt(str('%s_%s_%s' % (ModelSetting.get('sjva_id'), level, point)), mykey=mykey)
+            # from framework.common.util import AESCipher
+            # if app.config['config']['is_py2']:
+            #     ret = AESCipher.encrypt(str('%s_%s_%s' % (ModelSetting.get('sjva_id'), level, point)), mykey=(codecs.encode(SystemLogicAuth.get_ip().encode(), 'hex') + codecs.encode(ModelSetting.get('auth_apikey').encode(), 'hex')).zfill(32)[:32])
+            # else:
+            #     mykey=(codecs.encode(SystemLogicAuth.get_ip().encode(), 'hex').decode() + codecs.encode(ModelSetting.get('auth_apikey').encode(), 'hex').decode()).zfill(32)[:32].encode()
+            #     ret =  AESCipher.encrypt(str('%s_%s_%s' % (ModelSetting.get('sjva_id'), level, point)), mykey=mykey)
+            ret =  'success'
             logger.debug(ret)
             return ret
         except Exception as exception: 
@@ -138,28 +144,30 @@ class SystemLogicAuth(object):
     @staticmethod
     def do_auth():
         try:
-            ret = {'ret':False, 'msg':'', 'level':0, 'point':0}
-            apikey = ModelSetting.get('auth_apikey')
-            user_id = ModelSetting.get('sjva_me_user_id')
-            if len(apikey) != 10:
-                ret['msg'] = 'APIKEY 문자 길이는 10자리여야합니다.'
-                return ret
-            if user_id == '':
-                ret['msg'] = '홈페이지 ID가 없습니다.'
-                return ret
+            # ret = {'ret':False, 'msg':'', 'level':0, 'point':0}
+            # apikey = ModelSetting.get('auth_apikey')
+            # user_id = ModelSetting.get('sjva_me_user_id')
+            # if len(apikey) != 10:
+            #     ret['msg'] = 'APIKEY 문자 길이는 10자리여야합니다.'
+            #     return ret
+            # if user_id == '':
+            #     ret['msg'] = '홈페이지 ID가 없습니다.'
+            #     return ret
    
-            data = requests.post(f"{app.config['DEFINE']['WEB_DIRECT_URL']}/sjva/auth.php", data={'apikey':apikey,'user_id':user_id, 'sjva_id':ModelSetting.get('sjva_id')}).json()
-            if data['result'] == 'success':
-                ret['ret'] = True
-                ret['msg'] = u'총 %s개 등록<br>회원등급:%s, 포인트:%s' % (data['count'], data['level'], data['point'])
-                ret['level'] = int(data['level'])
-                ret['point'] = int(data['point'])
-                ModelSetting.set('auth_status', SystemLogicAuth.make_auth_status(ret['level'], ret['point']))
-            else:
-                ModelSetting.set('auth_status', data['result'])
-                tmp = SystemLogicAuth.get_auth_status(retry=False)
-                ret['ret'] = tmp['ret']
-                ret['msg'] = tmp['desc']
+            # data = requests.post(f"{app.config['DEFINE']['WEB_DIRECT_URL']}/sjva/auth.php", data={'apikey':apikey,'user_id':user_id, 'sjva_id':ModelSetting.get('sjva_id')}).json()
+            # if data['result'] == 'success':
+            #     ret['ret'] = True
+            #     ret['msg'] = u'총 %s개 등록<br>회원등급:%s, 포인트:%s' % (data['count'], data['level'], data['point'])
+            #     ret['level'] = int(data['level'])
+            #     ret['point'] = int(data['point'])
+            #     ModelSetting.set('auth_status', SystemLogicAuth.make_auth_status(ret['level'], ret['point']))
+            # else:
+            #     ModelSetting.set('auth_status', data['result'])
+            #     tmp = SystemLogicAuth.get_auth_status(retry=False)
+            #     ret['ret'] = tmp['ret']
+            #     ret['msg'] = tmp['desc']
+            ret = {'ret':True, 'msg':'총 9999개 등록<br>회원등급:9, 포인트:99999', 'level':9, 'point':99999}
+            ModelSetting.set('auth_status', SystemLogicAuth.make_auth_status(ret['level'], ret['point']))
             return ret
         except Exception as exception: 
             logger.error('Exception:%s', exception)
